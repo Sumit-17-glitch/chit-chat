2f5817e528c0f341eab4ce19f9a15515d1637e8f
Sumit-17-glitch <skkillgrave@gmail.com>
2026-07-08 17:51:27 +0530

    Add Tailwind CSS support and implement room management functionality
    - Added Tailwind CSS and its Vite plugin as dependencies in package.json and package-lock.json.
    - Created roomManager.js to handle WebSocket room functionalities including joining, leaving, and broadcasting messages.
    - Removed unused assets such as favicon.svg, icons.svg, hero.png, react.svg, and vite.svg from the frontend.

---
+++ chit-chat-backend/src/roomManager.js
@@ -0,0 +1,48 @@
+const rooms = new Map();
+
+export const joinRoom = (socket, roomName, userName) => {
+    if(!rooms.has(roomName)){
+        rooms.set(roomName, new Set())
+    }
+
+    const room = rooms.get(roomName);
+    room.add({
+        "socket": socket,
+        "userName": userName
+    });
+
+    socket.room = roomName;
+    console.log(`${userName} joined room`);
+    
+};
+
+export const leaveRoom = (socket) => {
+    const roomName = socket.room;
+
+    if(!roomName) return;
+
+    const room = rooms.get(roomName);
+
+    if(!room) return;
+
+    room.delete(socket);
+
+    if(room.size() == 0){
+        rooms.delete(room);
+    }
+
+    console.log("client left..");
+    
+}
+
+export const broadCastMeassage = (roomName, message) => {
+    const room = rooms.get(roomName);
+    if(!room) return;
+
+    room.forEach((client) => {
+        client.socket.send(message);
+    });
+}
+
+
+export {rooms}
